b44af158cbeb75b31e287e854a852c0d7befca77
timohuennebeck <hello@timohuennebeck.com>
2024-07-11 10:19:51 +0200

    change email

--- src/app/auth/emailConfirmation/page.tsx
+++ src/app/auth/emailConfirmation/page.tsx
@@ -8,21 +8,21 @@ const EmailConfirmationPage = () => {
     return (
         <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
             <div className="bg-white rounded-2xl shadow-lg border p-8 max-w-md w-full text-center">
-                <div className="flex justify-center mb-6">
+                <div className="flex justify-center mb-4">
                     <EnvelopeIcon className="size-12" />
                 </div>
-                <h2 className="text-2xl font-semibold mb-4">Check Your Email</h2>
+                <h2 className="text-2xl font-semibold mb-2">Check Your Email</h2>
                 <p className="text-gray-600 mb-6">
                     We've sent a confirmation link to Your email address. Please click the link
                     inside it to activate Your account.
                 </p>
-                <p className="text-sm text-gray-500 mb-4 italic">
+                <p className="text-sm text-gray-500 mb-12 italic">
                     If You don't see the email, please check Your spam folder.
                 </p>
 
                 <Link
                     href="/"
-                    className="block w-full bg-black text-white rounded-md py-2 hover:bg-gray-800 transition duration-300 mt-6"
+                    className="block w-full bg-black text-white rounded-md py-2 hover:bg-gray-800 transition duration-300"
                 >
                     Return to Home
                 </Link>
