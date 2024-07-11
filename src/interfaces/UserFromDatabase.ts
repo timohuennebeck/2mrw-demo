8c6c580645eaea0f0f2d15bbc55a342c69702e91
timohuennebeck <hello@timohuennebeck.com>
2024-07-11 14:40:15 +0200

    update user structure

--- src/interfaces/UserFromDatabase.ts
+++ src/interfaces/UserFromDatabase.ts
@@ -1,6 +1,8 @@
 export interface UserFromDatabase {
     id: number;
     created_at: number;
+    updated_at: number;
+    full_name: string;
     email: string;
     stripe_price_id: string;
     has_premium: boolean;
