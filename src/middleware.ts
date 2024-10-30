8ef5889bb95b76783379825bd15e8598d469fb34
timohuennebeck <hello@timohuennebeck.com>
2024-10-30 07:05:34 +0100

    improve middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,11 @@ import { FreeTrialStatus } from "@/enums/FreeTrialStatus";
 import { SubscriptionStatus } from "@/enums/SubscriptionStatus";
 import { createServerClient } from "@supabase/ssr";
 import { NextRequest as nextRequest, NextResponse as nextResponse } from "next/server";
-import { SupabaseClient, User } from "@supabase/supabase-js";
-import moment from "moment";
+import { User } from "@supabase/supabase-js";
 
 const HIDE_ON_PREMIUM_PLAN = ["/choose-pricing-plan"];
 
-const handleRedirection = async ({
+const _handleRedirection = async ({
     request,
     user,
 }: {
@@ -20,13 +19,10 @@ const handleRedirection = async ({
     }
 
     if (request.nextUrl.pathname.startsWith("/auth")) {
-        if (user) {
-            // redirects authenticated users to the dashboard
-            return nextResponse.redirect(new URL("/", request.url));
-        }
+        if (!user) return nextResponse.next(); // allow non-authenticated users to access auth pages
 
-        // allow non-authenticated users to access auth pages
-        return nextResponse.next();
+        // redirects authenticated users to the dashboard
+        return nextResponse.redirect(new URL("/", request.url));
     }
 
     if (!user) {
@@ -36,70 +32,6 @@ const handleRedirection = async ({
 
     // user is authenticated and accessing a non-auth route
     return null;
-};
-
-const getSubscriptionStatus = async (supabaseClient: SupabaseClient, user: User) => {
-    // uses metadata as source of truth to prevent making obessive amounts of api calls whenever the middleware triggers
-    if (user?.user_metadata?.subscription_status && user?.user_metadata?.free_trial_status) {
-        return {
-            subscription_status: user.user_metadata.subscription_status,
-            free_trial_status: user.user_metadata.free_trial_status,
-            free_trial_end_date: user.user_metadata.free_trial_end_date,
-        };
-    }
-
-    const { data: subscriptionData, error: subscriptionError } = await supabaseClient
-        .from("subscriptions")
-        .select("status")
-        .eq("user_id", user.id)
-        .single();
-
-    const { data: freeTrialData, error: freeTrialError } = await supabaseClient
-        .from("free_trials")
-        .select("status, end_date")
-        .eq("user_id", user.id)
-        .single();
-
-    if (!subscriptionError && !freeTrialError && subscriptionData && freeTrialData) {
-        await supabaseClient.auth.updateUser({
-            data: {
-                subscription_status: subscriptionData.status,
-                free_trial_status: freeTrialData.status,
-                free_trial_end_date: freeTrialData.end_date,
-            },
-        });
-
-        return {
-            subscription_status: subscriptionData.status,
-            free_trial_status: freeTrialData.status,
-            free_trial_end_date: freeTrialData.end_date,
-        };
-    }
-
-    const fallbackStatus = {
-        subscription_status: SubscriptionStatus.NOT_PURCHASED,
-        free_trial_status: FreeTrialStatus.NOT_STARTED,
-        free_trial_end_date: moment().toISOString(),
-    };
-
-    await supabaseClient.auth.updateUser({ data: fallbackStatus });
-
-    return fallbackStatus;
-};
-
-const updateExpiredFreeTrial = async (supabaseClient: SupabaseClient, user: User) => {
-    const { data, error } = await supabaseClient
-        .from("free_trials")
-        .update({ status: FreeTrialStatus.EXPIRED })
-        .eq("user_id", user.id);
-
-    if (!error && data) {
-        await supabaseClient.auth.updateUser({
-            data: {
-                free_trial_status: FreeTrialStatus.EXPIRED,
-            },
-        });
-    }
 };
 
 export const middleware = async (request: nextRequest) => {
@@ -130,35 +62,20 @@ export const middleware = async (request: nextRequest) => {
         data: { user },
     } = await supabaseClient.auth.getUser();
 
-    const routingResponse = await handleRedirection({ request, user });
+    const routingResponse = await _handleRedirection({ request, user });
 
     if (routingResponse) return routingResponse;
 
     if (!user) return nextResponse.next();
 
-    const { subscription_status, free_trial_status, free_trial_end_date } =
-        await getSubscriptionStatus(supabaseClient, user);
-
-    const isFreeTrialOngoing = () => {
-        if (!free_trial_end_date) return false;
-
-        const now = moment();
-        const endDate = moment(free_trial_end_date);
-
-        return now.isSameOrBefore(endDate);
-    };
-
-    if (free_trial_status === FreeTrialStatus.ACTIVE && !isFreeTrialOngoing()) {
-        await updateExpiredFreeTrial(supabaseClient, user);
-    }
-
-    const hasPremiumSubscription = subscription_status === SubscriptionStatus.ACTIVE;
-    const isOnFreeTrial = free_trial_status === FreeTrialStatus.ACTIVE && isFreeTrialOngoing();
+    const hasPremiumSubscription =
+        user.user_metadata?.subscription_status === SubscriptionStatus.ACTIVE;
+    const isOnFreeTrial = user.user_metadata?.free_trial_status === FreeTrialStatus.ACTIVE;
     const hasPremiumOrFreeTrial = hasPremiumSubscription || isOnFreeTrial;
 
     const currentPath = request.nextUrl.pathname;
 
-    if (hasPremiumOrFreeTrial) {
+    if (!hasPremiumOrFreeTrial) {
         return HIDE_ON_PREMIUM_PLAN.includes(currentPath)
             ? nextResponse.redirect(new URL("/", request.url))
             : nextResponse.next();
