032e1058bb5e4f9966b3d676d5de3a43d8eb096e
timohuennebeck <hello@timohuennebeck.com>
2024-11-02 10:02:09 +0100

    move preloader inside the providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,21 +1,51 @@
 "use client";
 
 import { ProductsProvider } from "@/context/ProductsContext";
-import { SessionProvider } from "@/context/SessionContext";
-import useUserInfoPreloader from "@/hooks/useUserInfoPreloader";
+import { SessionProvider, useSession } from "@/context/SessionContext";
 import { queryClient } from "@/lib/qClient/qClient";
+import { fetchUserFreeTrial, fetchUserSubscription } from "@/services/supabase/queries";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-    useUserInfoPreloader(); // loads initial data once the user is logged in
-
     return (
         <QueryClientProvider client={queryClient}>
             <SessionProvider>
-                <ProductsProvider>
-                    {children}
-                </ProductsProvider>
+                <PreloaderWrapper>
+                    <ProductsProvider>
+                        {children}
+                    </ProductsProvider>
+                </PreloaderWrapper>
             </SessionProvider>
         </QueryClientProvider>
     );
+};
+
+const PreloaderWrapper = ({ children }: { children: React.ReactNode }) => {
+    const { userIsLoggedIn, user } = useSession();
+
+    useEffect(() => {
+        const fetchInitialInformation = async () => {
+            if (!userIsLoggedIn || !user) return;
+
+            try {
+                await Promise.all([
+                    queryClient.prefetchQuery({
+                        queryKey: ["freeTrial", user.id],
+                        queryFn: () => fetchUserFreeTrial(user.id),
+                    }),
+                    queryClient.prefetchQuery({
+                        queryKey: ["subscription", user.id],
+                        queryFn: () => fetchUserSubscription(user.id),
+                    }),
+                ]);
+            } catch (error) {
+                console.error("Error fetching initial data:", error);
+            }
+        };
+
+        fetchInitialInformation();
+    }, [userIsLoggedIn, user]);
+
+    return children;
 };
