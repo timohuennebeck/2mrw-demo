318fa3011616425cdfec05e0f2d30e7558462f76
timohuennebeck <hello@timohuennebeck.com>
2024-11-03 16:33:50 +0100

    change font when disabled

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -44,7 +44,7 @@ function InputField({
                     placeholder={placeholder}
                     value={value}
                     onChange={(e) => onChange(e.target.value)}
-                    className={`w-full rounded-md border border-neutral-300 px-3 py-2.5 text-sm outline-neutral-800 ${type === "password" && "pr-10"} ${disabled && "bg-neutral-100"}`}
+                    className={`w-full rounded-md border border-neutral-300 px-3 py-2.5 text-sm outline-neutral-800 ${type === "password" && "pr-10"} ${disabled && "bg-neutral-100 text-neutral-400"}`}
                     onFocus={onFocus}
                     onBlur={onBlur}
                     disabled={disabled}
