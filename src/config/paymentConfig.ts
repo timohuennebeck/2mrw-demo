a6e8182c897499ac09396f07559a622003e2346f
timohuennebeck <hello@timohuennebeck.com>
2024-11-06 17:02:53 +0100

    organise config

--- src/config/paymentConfig.ts
+++ src/config/paymentConfig.ts
@@ -1,50 +1,41 @@
 import { BillingPlan } from "@/interfaces/StripePrices";
 
-interface SubscriptionSettings {
-    yearlyDiscountPercentage: number;
-}
-
-interface OneTimeSettings {}
-
 interface CompletePaymentConfig {
-    paymentType: BillingPlan;
     isFreePlanEnabled: boolean;
     currency: string;
-    subscriptionSettings: SubscriptionSettings;
-    oneTimeSettings: OneTimeSettings;
+    billingPlan: BillingPlan;
+    subscriptionSettings: {};
+    oneTimeSettings: {};
 }
 
-export const paymentConfig: CompletePaymentConfig = {
-    // TOOD: remove this as we now have billing_plan inside the database
-    paymentType: BillingPlan.RECURRING, // set which payment type is active
+const DEFAULT_CURRENCY = "EUR";
 
+export const paymentConfig: CompletePaymentConfig = {
     /**
      * if free plan is still shown after disabling it, reload the page
      * as it needs to refetch the products from the database
      */
-
     isFreePlanEnabled: true,
-    currency: "EUR",
+    currency: DEFAULT_CURRENCY,
+    billingPlan: BillingPlan.RECURRING,
 
-    // TODO: query the prices from the database where the billing_plan is equal to the billing_plan from the products table and the product_id is equal to the id from the products table
+    /**
+     * the subscriptionSettings and oneTimeSettings are work in progress
+     * and will be used in the latest update to enable / disable free trials and its duration
+     */
 
-    // subscription settings: used when paymentType is SUBSCRIPTION
-    subscriptionSettings: {
-        yearlyDiscountPercentage: 20,
-    },
-
-    // one-time settings: used when paymentType is ONE_TIME
+    subscriptionSettings: {},
     oneTimeSettings: {},
 };
 
 // helper functions to get the active settings
 export const getCurrentPaymentSettings = () => {
-    return paymentConfig.paymentType === BillingPlan.RECURRING
+    return paymentConfig.billingPlan === BillingPlan.RECURRING
         ? paymentConfig.subscriptionSettings
         : paymentConfig.oneTimeSettings;
 };
 
 // helper to check if subscription features are enabled
-export const isOneTimePaymentEnabled = () => paymentConfig.paymentType === BillingPlan.ONE_TIME;
 export const isFreePlanEnabled = () => paymentConfig.isFreePlanEnabled;
 export const getCurrency = () => paymentConfig.currency;
+export const isOneTimePaymentEnabled = () => paymentConfig.billingPlan === BillingPlan.ONE_TIME;
