3dc9f860a8560f2551d8dfe9adf9ea30a5170f92
timohuennebeck <hello@timohuennebeck.com>
2024-11-10 11:11:26 +0100

    add sign up logic

--- cypress/e2e/auth/sign-up.cy.ts
+++ cypress/e2e/auth/sign-up.cy.ts
@@ -43,11 +43,6 @@ describe("sign up", () => {
             cy.contains(TextConstants.ERROR__EMAIL_ALREADY_IN_USE).should("be.visible");
         });
 
-        it("should show password strength checker when focused", () => {
-            cy.get('[data-testid="password-input"]').should("be.visible").focus();
-            cy.get('[data-testid="password-strength-checker"]').should("be.visible");
-        });
-
         it("should show password is too weak message", () => {
             cy.get('[data-testid="first-name-input"]').should("be.visible").type("Timo");
             cy.get('[data-testid="email-input"]').should("be.visible").type("example@example.com");
@@ -57,21 +52,47 @@ describe("sign up", () => {
                 "be.visible",
             );
         });
+    });
 
-        it("should show password when clicking on icon", () => {
-            cy.get('[data-testid="password-input"]')
-                .should("have.attr", "type", "password")
-                .type("password");
+    it("should show password strength checker when focused", () => {
+        cy.get('[data-testid="password-input"]').should("be.visible").focus();
+        cy.get('[data-testid="password-strength-checker"]').should("be.visible");
+    });
 
-            cy.get('[data-testid="password-toggle"]').should("be.visible").click();
-            cy.get('[data-testid="password-input"]')
-                .should("have.attr", "type", "text")
-                .should("have.value", "password");
+    it("should show password when clicking on icon", () => {
+        cy.get('[data-testid="password-input"]')
+            .should("have.attr", "type", "password")
+            .type("password");
 
-            cy.get('[data-testid="password-toggle"]').should("be.visible").click();
+        cy.get('[data-testid="password-toggle"]').should("be.visible").click();
+        cy.get('[data-testid="password-input"]')
+            .should("have.attr", "type", "text")
+            .should("have.value", "password");
+
+        cy.get('[data-testid="password-toggle"]').should("be.visible").click();
+        cy.get('[data-testid="password-input"]')
+            .should("have.attr", "type", "password")
+            .should("have.value", "password");
+    });
+
+    describe("success flow", () => {
+        it.only("should show success message when signing up", () => {
+            cy.get('[data-testid="first-name-input"]').should("be.visible").type("Timo");
+
+            const randomlyGeneratedEmail = `example${Math.random().toString(36).substring(2, 15)}@example.com`;
+            cy.get('[data-testid="email-input"]').should("be.visible").type(randomlyGeneratedEmail);
+
+            const randomlyGeneratedPassword = `${Math.random().toString(36).substring(2, 15)}Hg1`;
             cy.get('[data-testid="password-input"]')
-                .should("have.attr", "type", "password")
-                .should("have.value", "password");
+                .should("be.visible")
+                .type(randomlyGeneratedPassword);
+
+            cy.get('[data-testid="sign-up-button"]').should("be.visible").click();
+            cy.contains(TextConstants.TEXT__SIGNUP_CONFIRMATION_SENT).should("be.visible");
+
+            cy.contains(TextConstants.TEXT__DIDNT_RECEIVE_EMAIL, { timeout: 5000 }).should(
+                "be.visible",
+            );
         });
     });
 });
