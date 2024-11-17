aba6b909fc4a9f4f4e41009fc17acc86ba1bcf8e
timohuennebeck <hello@timohuennebeck.com>
2024-11-17 10:55:39 +0100

    add missing sql tables

--- src/services/database/migrations/createTables.sql
+++ src/services/database/migrations/createTables.sql
@@ -1,18 +1,20 @@
-CREATE TYPE SubscriptionStatusEnums AS ENUM ('ACTIVE', 'EXPIRED', 'NOT_PURCHASED', 'CANCELLED');
+CREATE TYPE SubscriptionStatusEnums AS ENUM ('ACTIVE', 'TRIALING', 'CANCELLED', 'EXPIRED');
 
-CREATE TYPE SubscriptionTierEnums AS ENUM ('TIER_ZERO', 'TIER_ONE', 'TIER_TWO');
+CREATE TYPE SubscriptionTierEnums AS ENUM ('FREE', 'ESSENTIALS', 'FOUNDERS');
 
-CREATE TYPE BillingPlan AS ENUM ('SUBSCRIPTION', 'ONE_TIME');
+CREATE TYPE BillingPlan AS ENUM ('NONE', 'ONE_TIME', 'RECURRING');
+
+CREATE TYPE SubscriptionInterval AS ENUM ('NONE', 'MONTHLY', 'YEARLY');
 
 CREATE TABLE
     users (
-        id UUID PRIMARY KEY UNIQUE DEFAULT NOT NULL,
+        id UUID PRIMARY KEY UNIQUE DEFAULT gen_random_uuid (),
         stripe_customer_id TEXT,
-        profile_image_url TEXT,
         email TEXT NOT NULL,
-        first_name TEXT NOT NULL,
-        created_at TIMESTAMPTZ NOT NULL,
-        updated_at TIMESTAMPTZ NOT NULL
+        first_name TEXT,
+        profile_image_url TEXT,
+        created_at TIMESTAMPTZ,
+        updated_at TIMESTAMPTZ
     );
 
 CREATE TABLE
@@ -20,25 +22,38 @@ CREATE TABLE
         id UUID PRIMARY KEY UNIQUE DEFAULT gen_random_uuid (),
         name TEXT NOT NULL,
         description TEXT NOT NULL,
-        pricing JSONB NOT NULL,
         is_highlighted BOOLEAN NOT NULL,
         is_active BOOLEAN NOT NULL,
-        features JSONB NOT NULL,
         subscription_tier SubscriptionTierEnums NOT NULL,
-        created_at TIMESTAMPTZ NOT NULL,
-        updated_at TIMESTAMPTZ NOT NULL
+        billing_plan BillingPlan NOT NULL,
+        created_at TIMESTAMPTZ,
+        updated_at TIMESTAMPTZ
     );
 
 CREATE TABLE
     user_subscriptions (
         id UUID PRIMARY KEY UNIQUE DEFAULT gen_random_uuid (),
-        stripe_price_id TEXT NOT NULL,
-        stripe_subscription_id TEXT,
         user_id UNIQUE UUID REFERENCES public.users (id) ON DELETE CASCADE NOT NULL,
+        stripe_price_id TEXT REFERENCES public.stripe_prices (stripe_price_id) ON DELETE CASCADE NOT NULL,
+        stripe_subscription_id TEXT,
         status SubscriptionStatusEnums NOT NULL,
-        end_date TIMESTAMPTZ,
         subscription_tier SubscriptionTierEnums NOT NULL,
+        end_date TIMESTAMPTZ,
         billing_plan BillingPlan NOT NULL,
-        created_at TIMESTAMPTZ NOT NULL,
-        updated_at TIMESTAMPTZ NOT NULL
+        created_at TIMESTAMPTZ,
+        updated_at TIMESTAMPTZ
+    );
+
+CREATE TABLE
+    stripe_prices (
+        id UUID PRIMARY KEY UNIQUE DEFAULT gen_random_uuid (),
+        product_id UUID REFERENCES public.products (id) ON DELETE CASCADE NOT NULL,
+        subscription_interval SubscriptionInterval NOT NULL,
+        stripe_price_id TEXT NOT NULL,
+        current_amount NUMERIC NOT NULL,
+        previous_amount NUMERIC,
+        is_active BOOLEAN NOT NULL,
+        billing_plan BillingPlan NOT NULL,
+        created_at TIMESTAMPTZ,
+        updated_at TIMESTAMPTZ
     );
