83e05d2478bab84ce21eb40530d474cb25502a1c
timohuennebeck <hello@timohuennebeck.com>
2024-12-13 19:10:29 +0100

    remove unused helpers

--- src/utils/validators/formatValidator.ts
+++
@@ -1,5 +0,0 @@
-export const validateEmailFormat = (email: string) => {
-    // RFC 5322 compliant email regex
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailRegex.test(email);
-};
