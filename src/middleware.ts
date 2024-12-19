53a641599c29c6d1a02588cd0da5400cda246ba3
timohuennebeck <hello@timohuennebeck.com>
2024-12-19 17:47:10 +0100

    change config

--- src/middleware.ts
+++ src/middleware.ts
@@ -242,5 +242,14 @@ export const middleware = async (request: nextRequest) => {
 };
 
 export const config = {
-    matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+    matcher: [
+        /*
+         * This matches all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+
+        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    ],
 };
