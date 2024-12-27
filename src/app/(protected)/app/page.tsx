c06c542d9cee37a450c657d8fe96ea17bc6d12e2
timohuennebeck <hello@timohuennebeck.com>
2024-12-27 15:38:55 +0100

    add disclaimer

--- src/app/(protected)/app/page.tsx
+++ src/app/(protected)/app/page.tsx
@@ -4,6 +4,11 @@ const DemoPage = () => {
     return (
         <div className="flex h-full flex-col items-center justify-center">
             <p className="rounded-sm bg-gray-100 px-4 py-2 text-sm">
+                Pages such as the Documentation, Introduction, Settings, etc. are just for demo
+                purposes to show how the sidebar works.
+            </p>
+
+            <p className="rounded-sm bg-gray-100 px-4 py-2 text-sm mt-4">
                 You can go to the <span className="font-semibold">src/app/page.tsx</span> file to
                 edit this page
             </p>
