b07986472c1dc2a8ce9b403688bc248486259d7a
timohuennebeck <hello@timohuennebeck.com>
2024-12-27 15:41:06 +0100

    change layout

--- src/app/(protected)/app/page.tsx
+++ src/app/(protected)/app/page.tsx
@@ -4,13 +4,13 @@ const DemoPage = () => {
     return (
         <div className="flex h-full flex-col items-center justify-center">
             <p className="rounded-sm bg-gray-100 px-4 py-2 text-sm">
-                Pages such as the Documentation, Introduction, Settings, etc. are just for demo
-                purposes to show how the sidebar works.
-            </p>
-
-            <p className="rounded-sm bg-gray-100 px-4 py-2 text-sm mt-4">
                 You can go to the <span className="font-semibold">src/app/page.tsx</span> file to
                 edit this page
+            </p>
+
+            <p className="mt-4 rounded-sm bg-gray-100 px-4 py-2 text-center text-sm">
+                Disclaimer: Pages such as the Documentation, Introduction, Settings, etc. are just
+                for demo purposes to show how the sidebar works.
             </p>
         </div>
     );
