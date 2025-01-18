9256043d1247460a45454b2461c6283a7e22dd20
timohuennebeck <hello@timohuennebeck.com>
2025-01-18 21:01:48 +0100

    add fadetitle option

--- src/components/application/placeholder-state.tsx
+++ src/components/application/placeholder-state.tsx
@@ -5,6 +5,7 @@ import { LucideIcon } from "lucide-react";
 interface PlaceholderStateProps {
     title: string;
     description: string;
+    fadeTitle?: boolean;
     icon?: LucideIcon;
     iconStyles?: string;
     ctaLabel?: string;
@@ -14,6 +15,7 @@ interface PlaceholderStateProps {
 export const PlaceholderState = ({
     title,
     description,
+    fadeTitle,
     icon: Icon,
     iconStyles,
     ctaLabel,
@@ -28,7 +30,14 @@ export const PlaceholderState = ({
                 />
             )}
             <div className="flex flex-col items-center justify-center gap-1">
-                <h3 className="max-w-lg text-center text-base font-semibold">{title}</h3>
+                <h3
+                    className={cn(
+                        "max-w-lg text-center text-base font-semibold",
+                        fadeTitle && "text-muted-foreground",
+                    )}
+                >
+                    {title}
+                </h3>
                 <p className="max-w-sm text-center text-sm text-muted-foreground">{description}</p>
             </div>
             {ctaLabel && onClick && (
