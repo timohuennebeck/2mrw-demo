e05173908818e53142f2097496cabb6682ca842f
timohuennebeck <hello@timohuennebeck.com>
2025-02-02 15:39:00 +0100

    add missing border

--- src/data/marketing/stats-data.ts
+++ src/data/marketing/stats-data.ts
@@ -4,20 +4,23 @@ export const exampleStats = [
         label: "Lorem ipsum dolor sit amet.",
         description:
             "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo, enim!",
-        className: "bg-gray-100 dark:bg-background rounded-lg p-8",
+        className:
+            "bg-gray-100 dark:bg-background border border-gray-100 dark:border-border rounded-lg p-8",
     },
     {
         value: "€8.2 Billion",
         label: "Lorem ipsum dolor sit amet.",
         description:
             "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo, enim!",
-        className: "bg-gray-100 dark:bg-background rounded-lg p-8",
+        className:
+            "bg-gray-100 dark:bg-background border border-gray-100 dark:border-border rounded-lg p-8",
     },
     {
         value: "401,851",
         label: "Lorem ipsum dolor sit amet.",
         description:
             "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo, enim!",
-        className: "bg-gray-100 dark:bg-background rounded-lg p-8",
+        className:
+            "bg-gray-100 dark:bg-background border border-gray-100 dark:border-border rounded-lg p-8",
     },
 ];
